27fadd11a8c9504ceba2bfe85cbd12eb414f7a23
cardossosolutions <cardossosolutions@gmail.com>
2025-08-03 21:02:25 -0300

    Fix duplicate imports in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
-import ResetPasswordScreen from './components/auth/ResetPasswordScreen';
 import ResetPasswordScreen from './components/auth/ResetPasswordScreen';
 import { AuthProvider } from './contexts/AuthContext';
 import { ToastProvider } from './contexts/ToastContext';
@@ -16,11 +14,6 @@ createRoot(document.getElementById('root')!).render(
         <ToastProvider>
           <Routes>
             <Route path="/" element={<App />} />
-            <Route path="/reset-senha/:hash" element={<ResetPasswordScreen />} />
-          </Routes>
-        </ToastProvider>
-      </AuthProvider>
-    </Router>
             <Route path="/reset-senha/:hash" element={<ResetPasswordScreen />} />
           </Routes>
         </ToastProvider>
