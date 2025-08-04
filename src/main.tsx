bca1b2375cbf9393ea7c1c15fd879be8b01e6a03
cardossosolutions <cardossosolutions@gmail.com>
2025-08-03 21:01:20 -0300

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
+import ResetPasswordScreen from './components/auth/ResetPasswordScreen';
 import ResetPasswordScreen from './components/auth/ResetPasswordScreen';
 import { AuthProvider } from './contexts/AuthContext';
 import { ToastProvider } from './contexts/ToastContext';
@@ -14,6 +16,11 @@ createRoot(document.getElementById('root')!).render(
         <ToastProvider>
           <Routes>
             <Route path="/" element={<App />} />
+            <Route path="/reset-senha/:hash" element={<ResetPasswordScreen />} />
+          </Routes>
+        </ToastProvider>
+      </AuthProvider>
+    </Router>
             <Route path="/reset-senha/:hash" element={<ResetPasswordScreen />} />
           </Routes>
         </ToastProvider>
